f1d279582185febc431dd5d6fc2c5fc5d551f98d
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-10 00:32:01 +0900

    mod ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 23:50:13 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/09 20:48:11 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/10 00:31:38 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@
 static void	*trime_str(const char *str, int *sign)
 {
 	while (!ft_isdigit(*str) && *str != '+' && *str != '-')
+	{
+		if (!ft_isprint(*str))
+			return (NULL);
 		str++;
+	}	
 	*sign = 1;
 	if (*str == '-' || *str == '+')
 	{
@@ -59,8 +63,10 @@ int	ft_atoi(const char *str)
 	long	num_in_str;
 	int		sign;
 
-	str = (const char *)trime_str(str, &sign);
 	num_in_str = 0;
+	str = (const char *)trime_str(str, &sign);
+	if (str == NULL)
+		return (num_in_str);
 	while (ft_isdigit(*str))
 	{
 		num_in_str = atoi_helper(str, num_in_str, sign);
@@ -76,9 +82,10 @@ int	ft_atoi(const char *str)
 // #include <stdlib.h>
 
 // int main(void){
-	// char str[] = "-92233720368547758089";
-	// char str[] = "92233720368547758079";
-// 	char str[] = "-2147483649";
+// 	// char str[] = "-92233720368547758089";
+// 	// char str[] = "92233720368547758079";
+// 	// char str[] = "-2147483649";
+// 	char str[] = "06050";
 
 // 	printf("atoi :    %d\n", atoi(str));
 // 	printf("ft_atoi : %d\n", ft_atoi(str));
