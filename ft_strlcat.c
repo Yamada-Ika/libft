9c25cbec665764fa43afc18f5e4a99a3b448475a
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-10 00:24:29 +0900

    mod ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 19:59:00 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/10 00:02:32 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/10 00:24:06 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,29 +39,25 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 // #include <stdio.h>
 
 // int main(void){
-// 	char src[] = "the cake is a lie !\0I'm hidden lol\r\n";
-// 	char dst[3840] = "there is no stars in the sky";
-// 	char ft_dst[3840] = "there is no stars in the sky"; 
-// 	char s1[4] = "";
-// 	char s2[4] = "";
+// 	char src[] = "01234\0 5679";
+// 	char dst[3840] = "all digits : ";
+// 	char ft_dst[3840] = "all digits : "; 
 
 // 	// size_t len = strlen("the cake is a lie !\0I'm hidden lol\r\n") + 4;
 // 	size_t return_value;
 // 	size_t dstsize;
 
-// 	dstsize = strlen(dst);
+// 	dstsize = strlen(dst) + 10;
 // 	printf("dstsize = %zu\n", dstsize);
 // 	return_value = strlcat(dst, src, dstsize);
 // 	printf("strlcat    : %s return value : %zu\n", dst, return_value);
-// 	// for (size_t i = 0; i < return_value + 1; i++) {
-// 	// 	printf("dst[%02zu] = %02x : %c\n", i, dst[i], dst[i]);
-// 	// }
+// 	for (size_t i = 0; i < return_value + 10; i++) {
+// 		printf("dst[%02zu] = %02x : %c\n", i, dst[i], dst[i]);
+// 	}
 // 	printf("dstsize = %zu\n", dstsize);
 // 	return_value = ft_strlcat(ft_dst, src, dstsize);
 // 	printf("ft_strlcat : %s return value : %zu\n", ft_dst, return_value);
-// 	// for (size_t i = 0; i < return_value + 1; i++) {
-// 	// 	printf("dst[%02zu] = %02x : %c\n", i, ft_dst[i], ft_dst[i]);
-// 	// }
-// 	printf("strlcat return value    : %zu\n", strlcat(s1, "thx to ntoniolo for this test !", 4));
-// 	printf("ft_strlcat return value : %zu\n", ft_strlcat(s2, "thx to ntoniolo for this test !", 4));
+// 	for (size_t i = 0; i < return_value + 10; i++) {
+// 		printf("dst[%02zu] = %02x : %c\n", i, ft_dst[i], ft_dst[i]);
+// 	}
 // }
