68a94cd7cb6caab5bab1d9afd6083f26fef93f44
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-09 12:27:20 +0900

    mod ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 20:08:13 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/08 20:12:21 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/09 12:26:23 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ static char	*helper(const char *s1, const char *s2, size_t len, int i)
 	int		found_first_i;
 	char	*found_at;
 
+	found_at = NULL;
 	j = 0;
 	while (s1[i] != '\0' && len-- > 0)
 	{
@@ -45,24 +46,23 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	if (needle[0] == '\0')
 		return ((char *)haystack);
-	found_at = NULL;
 	found_at = helper(haystack, needle, len, 0);
 	return (found_at);
 }
 
 // -- test code --
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void){
-	char haystack[] = "see FF your FF return FF now FF";
-	char needle[] = "FF";
-	size_t len;
+// int main(void){
+// 	char haystack[] = "see FF your FF return FF now FF";
+// 	char needle[] = "FF";
+// 	size_t len;
 
-	len = strlen(haystack);
+	// len = strlen(haystack);
 	// printf("strnstr    : %p should be equal to %p\n", 
 	// 	strnstr(haystack, needle, len), &haystack[4]);
-	printf("strnstr    : %s\n",strnstr(haystack, needle, len));
+	// printf("strnstr    : %s\n",strnstr(haystack, needle, len));
 	// printf("ft_strnstr : %p should be equal to %p\n", 
 	// 	ft_strnstr(haystack, needle, len), &haystack[4]);
 	// printf("ft_strnstr : %s\n",strnstr(haystack, needle, len));
-}
+// }
