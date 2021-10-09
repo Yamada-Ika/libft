a505758d5b09f4215fe36b4bb83789f11f1c3550
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-10 02:46:44 +0900

    work if s1 or s2 is NULL pointer

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/10 02:46:03 by iyamada           #+#    #+#             */
+/*   Updated: 2021/10/10 02:46:03 by iyamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <stdlib.h>
 #include "libft.h"
@@ -12,17 +24,14 @@
 // 	return (count);
 // }
 
-static char	*createNullStr(size_t size)
+static char	*alloc_null_str(size_t size)
 {
 	char	*str;
-	int		i;
 
 	str = (char *)malloc(sizeof(size_t) * size);
 	if (str == NULL)
 		return (NULL);
-	i = 0;
-	while (size-- > 0)
-		str[i++] = '\0';
+	ft_bzero(str, size);
 	return (str);
 }
 
@@ -33,9 +42,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s2_len;
 	size_t	i;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	joined_str = createNullStr(s1_len + s2_len + 1);
+	joined_str = alloc_null_str(s1_len + s2_len + 1);
 	if (joined_str == NULL)
 		return (NULL);
 	i = 0;
@@ -55,7 +66,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 // #include <stdio.h>
 
 // int main(void) {
-// 	char s1[] = "Hello", s2[] = ", world!";
+// 	char s1[] = "", s2[] = "";
 // 	char *joined;
 
 // 	joined = ft_strjoin(s1, s2);
