c7f9b45e5c4aea4f572c86a35cdb8c0a5208f194
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-11 15:04:06 +0900

    mod ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:04 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/11 14:46:09 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/11 15:03:43 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,8 @@ void	*ft_calloc(size_t count, size_t size)
 
 	if (count == 0 || size == 0)
 	{
-		mem = malloc(1);
-		if (mem == NULL)
-			return (NULL);
-		ft_bzero(mem, 1);
-		return (mem);
+		count = 1;
+		size = 1;
 	}
 	mem = malloc(size * count);
 	if (mem == NULL)
@@ -39,43 +36,51 @@ void	*ft_calloc(size_t count, size_t size)
 // 	void *lib;
 // 	size_t count;
 
-// 	// < normal >
-// 	count = 10;
-// 	ft = ft_calloc(count, sizeof(int));
-// 	lib = calloc(count, sizeof(int));
-// 	printf("diff : %d\n", memcmp(ft, lib, count));
-// 	free(ft);
-// 	free(lib);
+	// // < normal >
+	// count = 10;
+	// ft = ft_calloc(count, sizeof(int));
+	// lib = calloc(count, sizeof(int));
+	// printf("diff : %d\n", memcmp(ft, lib, count));
+	// free(ft);
+	// free(lib);
 
-// 	// < size = 0 >
-// 	count = 10;
-// 	ft = ft_calloc(count, 0);
-// 	lib = calloc(count, 0);
-// 	printf("diff : %d\n", memcmp(ft, lib, 1));
-// 	free(ft);
-// 	free(lib);
+	// < empty string >
+	// count = 0;
+	// ft = ft_calloc(count, sizeof(char));
+	// lib = calloc(count, sizeof(char));
+	// printf("diff : %d\n", memcmp(ft, lib, 1));
+	// free(ft);
+	// free(lib);
 
-// 	// < count = 0 >
-// 	count = 1;
-// 	ft = ft_calloc(0, sizeof(int));
-// 	lib = calloc(0, sizeof(int));
-// 	printf("diff : %d\n", memcmp(ft, lib, count));
-// 	free(ft);
-// 	free(lib);
+	// // < size = 0 >
+	// count = 10;
+	// ft = ft_calloc(count, 0);
+	// lib = calloc(count, 0);
+	// printf("diff : %d\n", memcmp(ft, lib, 1));
+	// free(ft);
+	// free(lib);
 
-// 	// // < count = 0 size = (char) >
-// 	// count = 1;
-// 	// ft = ft_calloc(0, sizeof(char));
-// 	// lib = calloc(0, sizeof(char));
-// 	// printf("diff : %d\n", memcmp(ft, lib, count));
-// 	// free(ft);
-// 	// free(lib);
+	// // < count = 0 >
+	// count = 1;
+	// ft = ft_calloc(0, sizeof(int));
+	// lib = calloc(0, sizeof(int));
+	// printf("diff : %d\n", memcmp(ft, lib, count));
+	// free(ft);
+	// free(lib);
 
-// 	// < count = 1 >
-// 	count = 1;
-// 	ft = ft_calloc(count, sizeof(int));
-// 	lib = calloc(count, sizeof(int));
-// 	printf("diff : %d\n", memcmp(ft, lib, count));
-// 	free(ft);
-// 	free(lib);
+	// // // < count = 0 size = (char) >
+	// // count = 1;
+	// // ft = ft_calloc(0, sizeof(char));
+	// // lib = calloc(0, sizeof(char));
+	// // printf("diff : %d\n", memcmp(ft, lib, count));
+	// // free(ft);
+	// // free(lib);
+
+	// // < count = 1 >
+	// count = 1;
+	// ft = ft_calloc(count, sizeof(int));
+	// lib = calloc(count, sizeof(int));
+	// printf("diff : %d\n", memcmp(ft, lib, count));
+	// free(ft);
+	// free(lib);
 // }
