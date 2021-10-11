7825f81076b43288f4c4a84a2e967eb9428fc0bb
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-11 19:05:35 +0900

    use recursion

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,71 +6,100 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:29 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/11 00:06:51 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/11 19:04:55 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	get_digits(int n)
+static size_t	get_num_of_digits(int n)
 {
-	size_t	digit_num;
+	size_t	digit_count;
 
-	digit_num = 0;
+	digit_count = 0;
 	if (n == 0)
 		return (1);
 	if (n < 0)
-		digit_num += 1;
+		digit_count += 1;
 	while (n != 0)
 	{
-		digit_num++;
+		digit_count++;
 		n /= 10;
 	}
-	return (digit_num);
+	return (digit_count);
 }
 
-char	*itostr(int n, char *str, size_t str_len, size_t *i)
-{
-	size_t	flg_int_min;
+// static char	*itostr(int n, char *str, size_t str_len, size_t *i)
+// {
+// 	size_t	flg_int_min;
 
-	flg_int_min = 0;
+// 	flg_int_min = 0;
+// 	if (n < 0)
+// 	{
+// 		if (n == INT_MIN)
+// 		{
+// 			flg_int_min = 1;
+// 			n++;
+// 		}
+// 		n *= -1;
+// 		str[0] = '-';
+// 	}
+// 	while (*i < str_len)
+// 	{
+// 		str[str_len - (*i + 1)] = n % 10 + '0';
+// 		n /= 10;
+// 		if (n == 0)
+// 			break ;
+// 		*i += 1;
+// 	}
+// 	if (flg_int_min == 1)
+// 		str[str_len - 1] += 1;
+// 	str[str_len] = '\0';
+// 	return (str);
+// }
+
+static void	itostr(int n, char *str, size_t len, size_t offset)
+{
 	if (n < 0)
 	{
+		*(str - len + 1) = '-';
 		if (n == INT_MIN)
 		{
-			flg_int_min = 1;
+			offset = 1;
 			n++;
 		}
 		n *= -1;
-		str[0] = '-';
 	}
-	while (*i < str_len)
+	if (n == 0 && len == 1)
+		*str = n + '0';
+	else if (n == 0)
+		;
+	else
 	{
-		str[str_len - (*i + 1)] = n % 10 + '0';
-		n /= 10;
-		if (n == 0)
-			break ;
-		*i += 1;
+		if (offset == 1)
+			*str = n % 10 + '0' + offset;
+		else
+			*str = n % 10 + '0';
+		offset = 0;
+		itostr(n / 10, --str, len, offset);
 	}
-	if (flg_int_min == 1)
-		str[str_len - 1] += 1;
-	str[str_len] = '\0';
-	return (str);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*stringfied_i;
-	size_t	str_i_len;
-	size_t	i;
+	char	*str;
+	size_t	i_len;
+	// size_t	i;
 
-	str_i_len = get_digits(n);
-	stringfied_i = (char *)malloc(sizeof(char) * (str_i_len + 1));
-	if (stringfied_i == NULL)
+	i_len = get_num_of_digits(n);
+	str = (char *)malloc(sizeof(char) * (i_len + 1));
+	if (str == NULL)
 		return (NULL);
-	i = 0;
-	stringfied_i = itostr(n, stringfied_i, str_i_len, &i);
-	return (stringfied_i);
+	// i = 0;
+	// str = itostr(n, str, str_i_len, &i);
+	*(str + i_len) = '\0';
+	itostr(n, str + i_len - 1, i_len, 0);
+	return (str);
 }
 
 // -- test code --
@@ -84,6 +113,9 @@ char	*ft_itoa(int n)
 // 	printf("%s\n", return_value);
 // 	free(return_value);
 // 	return_value = ft_itoa(1);
+// 	printf("%s\n", return_value);
+// 	free(return_value);
+// 	return_value = ft_itoa(12345);
 // 	printf("%s\n", return_value);
 // 	free(return_value);
 // 	return_value = ft_itoa(-1);
