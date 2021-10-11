4ca8d9af46c1072b8adae2e619a7e8b0e907b405
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-11 14:36:14 +0900

    add return statement

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:04 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/11 13:54:55 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/11 14:35:33 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	*ft_calloc(size_t count, size_t size)
 		if (alloc_mem == NULL)
 			return (NULL);
 		ft_bzero(alloc_mem, 1);
+		return (alloc_mem);
 	}
 	alloc_mem = malloc(size * count);
 	if (alloc_mem == NULL)
@@ -34,11 +35,39 @@ void	*ft_calloc(size_t count, size_t size)
 // #include <stdio.h>
 
 // int main(void) {
-// 	int *hoge;
+// 	void *ft;
+// 	void *lib;
+// 	size_t count;
 
-// 	hoge = ft_calloc(10, sizeof(int));
-// 	for (int i = 0; i < 10; i++) {
-// 		printf("hoge[%d] = %d\n", i, hoge[i]);
-// 	}
-// 	free(hoge);
+// 	// < normal >
+// 	count = 10;
+// 	ft = ft_calloc(count, sizeof(int));
+// 	lib = calloc(count, sizeof(int));
+// 	printf("diff : %d\n", memcmp(ft, lib, count));
+// 	free(ft);
+// 	free(lib);
+
+// 	// < size = 0 >
+// 	count = 10;
+// 	ft = ft_calloc(count, 0);
+// 	lib = calloc(count, 0);
+// 	printf("diff : %d\n", memcmp(ft, lib, count));
+// 	free(ft);
+// 	free(lib);
+
+// 	// < count = 0 >
+// 	count = 1;
+// 	ft = ft_calloc(0, sizeof(int));
+// 	lib = calloc(0, sizeof(int));
+// 	printf("diff : %d\n", memcmp(ft, lib, count));
+// 	free(ft);
+// 	free(lib);
+
+// 	// // < count = 0 size = (char) >
+// 	// count = 1;
+// 	// ft = ft_calloc(0, sizeof(char));
+// 	// lib = calloc(0, sizeof(char));
+// 	// printf("diff : %d\n", memcmp(ft, lib, count));
+// 	// free(ft);
+// 	// free(lib);
 // }
