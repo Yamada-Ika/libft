b6d95aa04d2e41fa04efb0bf739d147a36233d25
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-11 11:56:17 +0900

    mod calloc and memmove

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:04 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/11 00:12:54 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/11 11:35:01 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,18 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	int		*allocatedMemory;
-	void	*allocatedMemoryPointTo;
+	// void	*allocatedMemoryPointTo;
 
-	allocatedMemory = (int *)malloc(size * count);
+	// allocatedMemory = (int *)malloc(size * count);
+	allocatedMemory = malloc(size * count);
 	if (allocatedMemory == NULL)
 		return (NULL);
-	allocatedMemoryPointTo = (void *)allocatedMemory;
-	while (count-- > 0)
-		*(allocatedMemory++) = 0;
-	return (allocatedMemoryPointTo);
+	// allocatedMemoryPointTo = (void *)allocatedMemory;
+	ft_bzero(allocatedMemory, count);
+	// while (count-- > 0)
+	// 	*(allocatedMemory++) = 0;
+	// return (allocatedMemoryPointTo);
+	return (allocatedMemory);
 }
 
 // test code
