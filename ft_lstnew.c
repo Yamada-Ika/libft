dcb6ffeeed21078fce3f6dcafe79690b20c415e9
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-12 15:49:31 +0900

    add ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 15:33:49 by iyamada           #+#    #+#             */
+/*   Updated: 2021/10/12 15:49:12 by iyamada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void	*content)
+{
+	t_list	*list;
+
+	if (content == NULL)
+		return (NULL);
+	list = (t_list *)malloc(sizeof(t_list));
+	if (list == NULL)
+		return (NULL);
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
+
+// < test code >
+// #include <stdio.h>
+
+// int main(void) {
+// 	char *str1 = "hello, world!";
+// 	t_list	*greeting;
+
+// 	greeting = ft_lstnew(str1);
+// 	printf("%s %p\n", (char *)greeting->content, greeting->next);
+// 	free(greeting);
+// }
