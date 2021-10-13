4e47d35be40e8844a5c1d4cea87535fe399e45a8
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-14 00:59:57 +0900

    unit test ok

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:46:06 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/13 10:56:19 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/14 00:58:51 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,38 +16,33 @@
 t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
+	t_list	*prev_new;
 	t_list	*new_ptr;
 	t_list	*lst_ptr;
 
 	if (lst == NULL)
 		return (NULL);
-	new = ft_lstnew((*f)(lst->content));
-	if (new == NULL)
-		return (NULL);
+	prev_new = ft_lstnew((*f)(lst->content));
+		if (prev_new == NULL)
+			return (NULL);
+	new_ptr = prev_new;
 	lst_ptr = lst;
-	while (lst != NULL)
-	{
-		lst->content = (*f)(lst->content);
-		printf("lst->content : %s\n", (char *)lst->content);
-		lst = lst->next;
-	}
-	new_ptr = new;
-	ft_lstadd_back(&new, lst_ptr->next);
-	new = new_ptr;
-	while (new_ptr != NULL)
-	{
-		new_ptr->content = (*f)(new_ptr->content);
-		printf("new_ptr->content : %s\n", (char *)new_ptr->content);
-		new_ptr = new_ptr->next;
-	}
 	while (lst_ptr != NULL)
 	{
-		// new->content = (*f)(lst_ptr->content);
-		if ((*del) != NULL)
-			(*del)(lst_ptr->content);
-		// new->next = lst_ptr->next;
 		lst_ptr = lst_ptr->next;
-		// new = new->next;
+		if (lst_ptr == NULL)
+			break ;
+		new = ft_lstnew((*f)(lst_ptr->content));
+		if (new == NULL)
+			return (NULL);
+		prev_new->next = new;
+		prev_new = new;
 	}
-	return (new);
+	lst_ptr = lst;
+	while (lst_ptr != NULL && (*del) != NULL)
+	{
+		(*del)(lst_ptr->content);
+		lst_ptr = lst_ptr->next;
+	}
+	return (new_ptr);
 }
