3d7a8171ae767b464e524ab463cea34dcf67b1b3
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-14 17:38:32 +0900

    mod

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:14:48 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/14 17:26:29 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/14 17:38:18 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,13 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	t_list	*lst_last;
 	t_list	*lst_ptr;
 
-	if (lst == NULL || *lst == NULL)
+	if (lst == NULL)
 		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
 	lst_ptr = *lst;
 	lst_last = ft_lstlast(*lst);
 	lst_last->next = new;
