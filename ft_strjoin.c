f21b5cd719b7cf01d3a42b98e8e90c5720a3b698
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-14 10:37:32 +0900

    refact with using ft_memcpy

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,48 +6,31 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 02:46:03 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/11 00:11:07 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/14 10:36:16 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*alloc_null_str(size_t size)
-{
-	char	*str;
-
-	str = (char *)malloc(sizeof(size_t) * size);
-	if (str == NULL)
-		return (NULL);
-	ft_bzero(str, size);
-	return (str);
-}
-
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*joined_str;
+	char	*joined_str_ptr;
 	size_t	s1_len;
 	size_t	s2_len;
-	size_t	i;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	joined_str = alloc_null_str(s1_len + s2_len + 1);
+	joined_str = ft_calloc(s1_len + s2_len + 1, sizeof(char));
 	if (joined_str == NULL)
 		return (NULL);
-	i = 0;
-	while (i < s1_len + s2_len)
-	{
-		if (i < s1_len)
-			joined_str[i] = s1[i];
-		else
-			joined_str[i] = s2[i - s1_len];
-		i++;
-	}
-	joined_str[i] = '\0';
-	return (joined_str);
+	joined_str_ptr = joined_str;
+	joined_str = ft_memcpy(joined_str, s1, s1_len);
+	joined_str = ft_memcpy(joined_str + s1_len, s2, s2_len);
+	joined_str_ptr[s1_len + s2_len] = '\0';
+	return (joined_str_ptr);
 }
 
 // -- test code --
