4568089d8802f6b8eea5cd738844adfb813e1381
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-15 12:55:57 +0900

    mod following with h-san

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:46:06 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/15 12:50:00 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/15 12:55:25 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,37 +15,6 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	// t_list	*new;
-	// t_list	*next_new;
-	// t_list	*new_ptr;
-	// t_list	*lst_ptr;
-
-	// if (lst == NULL)
-	// 	return (NULL);
-	// new = ft_lstnew((*f)(lst->content));
-	// if (new == NULL)
-	// 	return (NULL);
-	// new_ptr = new;
-	// lst_ptr = lst;
-	// while (lst_ptr != NULL)
-	// {
-	// 	lst_ptr = lst_ptr->next;
-	// 	if (lst_ptr == NULL)
-	// 		break ;
-	// 	next_new = ft_lstnew((*f)(lst_ptr->content));
-	// 	if (next_new == NULL)
-	// 		return (NULL);
-	// 	ft_lstadd_back(&new, next_new);
-	// }
-	// lst_ptr = lst;
-	// while (lst_ptr != NULL && (*del) != NULL)
-	// {
-	// 	// printf("(*del) : %p\n", (*del));
-	// 	(*del)(lst_ptr->content);
-	// 	lst_ptr = lst_ptr->next;
-	// }
-	// return (new_ptr);
-
 	t_list	*new;
 	t_list	*next_new;
 	t_list	*new_ptr;
