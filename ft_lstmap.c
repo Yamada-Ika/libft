fa5a7565ebbb32990571ed2aee2fa808ff533222
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-18 17:04:39 +0900

    refact

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:46:06 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/16 18:40:13 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/18 17:03:40 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
+	t_list	**new_adr;
 	t_list	*next_new;
 
 	if (lst == NULL)
 		return (NULL);
 	new = NULL;
+	new_adr = &new;
 	while (lst != NULL)
 	{
 		next_new = ft_lstnew((*f)(lst->content));
@@ -29,7 +31,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 			return (NULL);
 		}
 		ft_lstadd_back(&new, next_new);
+		new = new->next;
 		lst = lst->next;
 	}
-	return (new);
+	return (*new_adr);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 12:23:10 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/18 12:58:47 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/18 16:49:02 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,56 +21,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 	if (s1 == NULL || set == NULL)
 		return (NULL);
 	head = s1;
-	while (ft_strchr(set, (int)*head))
+	while (*head != '\0' && ft_strchr(set, (int)*head))
 		head++;
 	tail = s1 + ft_strlen(s1);
-	while (ft_strchr(set, (int)*tail) && head <= tail)
+	while (tail < head && ft_strchr(set, (int)*tail))
 		tail--;
-	printf("head : %p tail : %p\n", head, tail);
-	return (ft_substr(head, 0, tail - head + 1));
+	return (ft_substr(head, 0, tail - head));
 }
 
 // -- test code --
 // #include <stdio.h>
 
 // int main(void) {
-// 	char s[] = "1234AAA22331122";
-// 	char set[] = "1234";
-// 	char s1[] = "\t'#9' #include <stdio.h>\t'#9'";
-// 	char set1[] = "\t";
-// 	char s2[] = "  \t \t \n   \n\n\n\t";
-// 	char set2[] = " \n\t";
 // 	char *trimed_s;
 
-	// // <- nortion case ->
-	// trimed_s = ft_strtrim(s, set);
-	// printf("trimed_s = %s\n", trimed_s);
-	// for (int i = 0; i < 4; i++)
-	// 	printf("%02x ", trimed_s[i]);
-	// putchar('\n');
-	// free(trimed_s);
-
-	// // <- fully trimmed ->
-	// trimed_s = ft_strtrim(s2, set2);
-	// printf("trimed_s = %s\n", trimed_s);
-	// for (int i = 0; i < 4; i++)
-	// 	printf("%02x ", trimed_s[i]);
-	// putchar('\n');
-	// free(trimed_s);
-
-	// // <- teraterm case ->
-	// trimed_s = ft_strtrim(s1, set1);
-	// printf("trimed_s = %s\n", trimed_s);
-	// for (int i = 0; i < strlen("#include <stdio.h>"); i++)
-	// 	printf("%02x ", trimed_s[i]);
-	// putchar('\n');
-	// free(trimed_s);
-
-	// // <- teraterm case ->
-	// trimed_s = ft_strtrim(s2, set2);
-	// printf("trimed_s = %s\n", trimed_s);
-	// for (int i = 0; i < strlen("#include <stdio.h>"); i++)
-	// 	printf("%02x ", trimed_s[i]);
-	// putchar('\n');
-	// free(trimed_s);
+// 	// <- nortion case ->
+// 	trimed_s = ft_strtrim("12\0f34aaa12\034", "12\0f34");
+// 	printf("trimed_s = %s\n", trimed_s);
+// 	for (int i = 0; i < 4; i++)
+// 		printf("%02x ", trimed_s[i]);
+// 	putchar('\n');
+// 	free(trimed_s);
 // }
