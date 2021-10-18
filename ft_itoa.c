0cb5eb902bba1a52a1ceb17dc5abcbf4958e7103
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-18 15:43:42 +0900

    do not use long long

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,109 +6,82 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:29 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/16 00:17:54 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/18 15:30:35 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	get_num_of_digits_helper(int n, size_t digit_count)
+static char	*ft_itoa_helper(int n, int digi_count, int is_nega, int is_INT_MIN)
 {
-	if (n == 0 && digit_count == 0)
-		return (1);
-	else if (n == 0)
-		return (digit_count);
-	if (n < 0)
-	{
-		if (n == INT_MIN)
-			n++;
-		n *= -1;
-		get_num_of_digits_helper(n, ++digit_count);
-	}
-	return (get_num_of_digits_helper(n / 10, ++digit_count));
-}
+	int		tmp_n;
+	char	*num;
 
-static size_t	get_num_of_digits(int n)
-{
-	return (get_num_of_digits_helper(n, 0));
-}
-
-static void	ft_itoa_helper(int n, char *str, size_t len, size_t offset)
-{
-	if (n < 0)
+	tmp_n = n;
+	while (tmp_n > 0)
 	{
-		*(str - len + 1) = '-';
-		if (n == INT_MIN)
-		{
-			offset = 1;
-			n++;
-		}
-		n *= -1;
+		digi_count++;
+		tmp_n /= 10;
 	}
-	if (n == 0 && len == 1)
-		*str = n + '0';
-	else if (n == 0)
-		return ;
-	else
+	num = (char *)ft_calloc(digi_count + 1, sizeof(char));
+	if (num == NULL)
+		return (NULL);
+	while (digi_count-- > 0)
 	{
-		if (offset == 1)
-			*str = n % 10 + '0' + offset;
-		else
-			*str = n % 10 + '0';
-		offset = 0;
-		ft_itoa_helper(n / 10, --str, len, offset);
+		num[digi_count] = n % 10 + is_INT_MIN + '0';
+		n /= 10;
+		is_INT_MIN = 0;
 	}
+	if (is_nega)
+		num[0] = '-';
+	return (num);
 }
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	size_t	i_len;
+	int	is_negative;
+	int	is_INT_MIN;
+	int	digits_count;
 
-	i_len = get_num_of_digits(n);
-	str = (char *)malloc(sizeof(char) * (i_len + 1));
-	if (str == NULL)
-		return (NULL);
-	*(str + i_len) = '\0';
-	ft_itoa_helper(n, str + i_len - 1, i_len, 0);
-	return (str);
+	if (n == 0)
+		return (ft_strdup("0"));
+	is_negative = 0;
+	is_INT_MIN = 0;
+	digits_count = 0;
+	if (n < 0)
+	{
+		is_negative = 1;
+		digits_count = 1;
+		if (n == INT_MIN)
+			is_INT_MIN = 1;
+		n = (n + is_INT_MIN) * -1;
+	}
+	return (ft_itoa_helper(n, digits_count, is_negative, is_INT_MIN));
 }
 
 // -- test code --
-// #include <stdio.h>
-// #include <limits.h>
+#include <stdio.h>
+#include <limits.h>
 
-// int main(void){
-// 	char *return_value;
+int main(void){
+	char *ret;
 
-// 	return_value = ft_itoa(0);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(1);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(12345);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(-1);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(-12);
-// 	printf("%s\n", return_value);
-// 	return_value = ft_itoa(12);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(-12);
-// 	printf("%s\n", return_value);
-// 	return_value = ft_itoa(123);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(-123);
-// 	printf("%s\n", return_value);
-// 	return_value = ft_itoa(INT_MAX);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// 	return_value = ft_itoa(INT_MIN);
-// 	printf("%s\n", return_value);
-// 	free(return_value);
-// }
+	ret = ft_itoa(0);
+	printf("%s\n", ret);
+	free(ret);
+	// ret = ft_itoa(1);
+	// printf("%s\n", ret);
+	// free(ret);
+	ret = ft_itoa(12);
+	printf("%s\n", ret);
+	free(ret);
+	ret = ft_itoa(-12);
+	printf("%s\n", ret);
+	free(ret);
+	ret = ft_itoa(INT_MIN);
+	printf("%s\n", ret);
+	free(ret);
+	ret = ft_itoa(INT_MAX);
+	printf("%s\n", ret);
+	free(ret);
+}
