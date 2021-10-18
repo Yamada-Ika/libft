78c6d191de8610b21a90a0e2aa43d57c2adc4d1c
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-18 15:48:04 +0900

    use long long

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,16 +6,16 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:29 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/18 15:30:35 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/18 15:47:48 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_itoa_helper(int n, int digi_count, int is_nega, int is_INT_MIN)
+static char	*ft_itoa_helper(long long n, int digi_count, int is_nega)
 {
-	int		tmp_n;
-	char	*num;
+	long long	tmp_n;
+	char		*num;
 
 	tmp_n = n;
 	while (tmp_n > 0)
@@ -28,9 +28,8 @@ static char	*ft_itoa_helper(int n, int digi_count, int is_nega, int is_INT_MIN)
 		return (NULL);
 	while (digi_count-- > 0)
 	{
-		num[digi_count] = n % 10 + is_INT_MIN + '0';
+		num[digi_count] = n % 10 + '0';
 		n /= 10;
-		is_INT_MIN = 0;
 	}
 	if (is_nega)
 		num[0] = '-';
@@ -40,48 +39,46 @@ static char	*ft_itoa_helper(int n, int digi_count, int is_nega, int is_INT_MIN)
 char	*ft_itoa(int n)
 {
 	int	is_negative;
-	int	is_INT_MIN;
 	int	digits_count;
+	long long	ll_n;
 
 	if (n == 0)
 		return (ft_strdup("0"));
 	is_negative = 0;
-	is_INT_MIN = 0;
 	digits_count = 0;
-	if (n < 0)
+	ll_n = (long long)n;
+	if (ll_n < 0)
 	{
 		is_negative = 1;
 		digits_count = 1;
-		if (n == INT_MIN)
-			is_INT_MIN = 1;
-		n = (n + is_INT_MIN) * -1;
+		ll_n *= -1;
 	}
-	return (ft_itoa_helper(n, digits_count, is_negative, is_INT_MIN));
+	return (ft_itoa_helper(ll_n, digits_count, is_negative));
 }
 
 // -- test code --
-#include <stdio.h>
-#include <limits.h>
+// #include <stdio.h>
+// #include <limits.h>
 
-int main(void){
-	char *ret;
+// int main(void){
+// 	char *ret;
 
-	ret = ft_itoa(0);
-	printf("%s\n", ret);
-	free(ret);
-	// ret = ft_itoa(1);
-	// printf("%s\n", ret);
-	// free(ret);
-	ret = ft_itoa(12);
-	printf("%s\n", ret);
-	free(ret);
-	ret = ft_itoa(-12);
-	printf("%s\n", ret);
-	free(ret);
-	ret = ft_itoa(INT_MIN);
-	printf("%s\n", ret);
-	free(ret);
-	ret = ft_itoa(INT_MAX);
-	printf("%s\n", ret);
-	free(ret);
-}
+// 	ret = ft_itoa(0);
+// 	printf("%s\n", ret);
+// 	free(ret);
+// 	ret = ft_itoa(1);
+// 	printf("%s\n", ret);
+// 	free(ret);
+// 	ret = ft_itoa(12);
+// 	printf("%s\n", ret);
+// 	free(ret);
+// 	ret = ft_itoa(-12);
+// 	printf("%s\n", ret);
+// 	free(ret);
+// 	ret = ft_itoa(INT_MIN);
+// 	printf("%s\n", ret);
+// 	free(ret);
+// 	ret = ft_itoa(INT_MAX);
+// 	printf("%s\n", ret);
+// 	free(ret);
+// }
