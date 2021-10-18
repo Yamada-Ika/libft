eff946158e68cab172ca4f0307bcf351128a7d3b
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-18 17:35:07 +0900

    mod

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,27 +6,31 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 12:23:10 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/18 17:08:21 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/18 17:34:54 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char const	*head;
 	char const	*tail;
+	size_t		trimmed_str_len;
 
 	if (s1 == NULL || set == NULL)
 		return (NULL);
 	head = s1;
 	while (*head != '\0' && ft_strchr(set, (int)*head))
 		head++;
-	tail = s1 + ft_strlen(s1);
-	while (tail < head && ft_strchr(set, (int)*tail))
+	tail = s1 + ft_strlen(s1) - 1;
+	while (ft_strchr(set, (int)*tail))
 		tail--;
-	return (ft_substr(head, 0, tail - head + 1));
+	if (*head == '\0')
+		trimmed_str_len = 0;
+	else
+		trimmed_str_len = tail - head + 1;
+	return (ft_substr(head, 0, trimmed_str_len));
 }
 
 // -- test code --
@@ -36,7 +40,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 // 	char *trimed_s;
 
 // 	// <- nortion case ->
-// 	trimed_s = ft_strtrim("12\0f34aaa12\034", "12\0f34");
+// 	trimed_s = ft_strtrim("1234aaa12", "1234");
 // 	printf("trimed_s = %s\n", trimed_s);
 // 	for (int i = 0; i < 4; i++)
 // 		printf("%02x ", trimed_s[i]);
