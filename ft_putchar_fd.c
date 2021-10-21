9f6bc8c13106529d41150a27c6c85f2a0a73a037
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-21 18:17:57 +0900

    delete val

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:47:30 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/12 10:39:47 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/21 18:17:34 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,13 @@ void	ft_putchar_fd(char c, int fd)
 {
 	unsigned char	head;
 	unsigned char	tail;
-	unsigned char	mask1;
-	unsigned char	mask2;
 
-	if ((unsigned char)c <= 0x7f)
+	if ((unsigned char)c <= 127)
 		write(fd, &c, sizeof(c));
 	else
 	{
-		mask1 = 0b00111111;
-		mask2 = 0b11000000;
-		head = 0b11000000 | (((unsigned char)c & mask2) >> 6);
-		tail = 0b10000000 | ((unsigned char)c & mask1);
+		head = 0b11000000 | (((unsigned char)c & 0b11000000) >> 6);
+		tail = 0b10000000 | ((unsigned char)c & 0b00111111);
 		write(fd, &head, sizeof(char));
 		write(fd, &tail, sizeof(char));
 	}
@@ -37,3 +33,5 @@ void	ft_putchar_fd(char c, int fd)
 // 	ft_putchar_fd(0x7a, 1);
 // 	ft_putchar_fd(0xa9, 1);
 // }
+
+// https://ja.wikipedia.org/wiki/Unicode%E4%B8%80%E8%A6%A7_0000-0FFF
