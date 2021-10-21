68995f56c6852ac63523f93820587e8c1d540b62
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-21 18:19:28 +0900

    rename prototype

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -6,13 +6,13 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:47:27 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/11 00:08:56 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/21 18:18:59 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putendl_fd(char const *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
