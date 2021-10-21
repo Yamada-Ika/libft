2a5c657ec07db6dba2f8109f3f254a9052416fd7
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2021-10-21 16:26:31 +0900

    delete comment

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,56 +6,12 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:46:29 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/21 16:25:32 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/10/21 16:26:18 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-
-// static char	*ft_itoa_helper(long long n, int digi_count, int is_nega)
-// {
-// 	long long	tmp_n;
-// 	char		*num;
-
-// 	tmp_n = n;
-// 	while (tmp_n > 0)
-// 	{
-// 		digi_count++;
-// 		tmp_n /= 10;
-// 	}
-// 	num = (char *)ft_calloc(digi_count + 1, sizeof(char));
-// 	if (num == NULL)
-// 		return (NULL);
-// 	while (digi_count-- > 0)
-// 	{
-// 		num[digi_count] = n % 10 + '0';
-// 		n /= 10;
-// 	}
-// 	if (is_nega)
-// 		num[0] = '-';
-// 	return (num);
-// }
-
-// char	*ft_itoa(int n)
-// {
-// 	int			is_negative;
-// 	int			digits_count;
-// 	long long	ll_n;
-
-// 	if (n == 0)
-// 		return (ft_strdup("0"));
-// 	is_negative = 0;
-// 	digits_count = 0;
-// 	ll_n = (long long)n;
-// 	if (ll_n < 0)
-// 	{
-// 		is_negative = 1;
-// 		digits_count = 1;
-// 		ll_n *= -1;
-// 	}
-// 	return (ft_itoa_helper(ll_n, digits_count, is_negative));
-// }
 
 static size_t	count_digits(int n)
 {
