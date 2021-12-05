a8b1c2455c41ad6d622d82dc522b63081cf4a451
Yamada_Ika <yamadaiori@yamadaiorinoMacBook-Air.local>
2021-12-06 00:52:02 +0900

    Remove mulit byte

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/10 23:47:30 by iyamada           #+#    #+#             */
-/*   Updated: 2021/10/25 10:55:31 by iyamada          ###   ########.fr       */
+/*   Updated: 2021/12/06 00:50:27 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,5 @@
 
 void	ft_putchar_fd(char c, int fd)
 {
-	unsigned char	head;
-	unsigned char	tail;
-
-	if ((unsigned char)c <= 127)
-		write(fd, &c, sizeof(c));
-	else
-	{
-		head = 0b11000000 | (((unsigned char)c & 0b11000000) >> 6);
-		tail = 0b10000000 | ((unsigned char)c & 0b00111111);
-		write(fd, &head, sizeof(char));
-		write(fd, &tail, sizeof(char));
-	}
+	write(fd, &c, sizeof(c));
 }
