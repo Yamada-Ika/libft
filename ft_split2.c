49811fb60c85e27d54961ead22acdd6edb93aaf6
Yamada-Ika <73375669+Yamada-Ika@users.noreply.github.com>
2022-05-09 17:08:17 +0900

    fix

--- ft_split2.c
+++ ft_split2.c
@@ -6,22 +6,62 @@
 /*   By: iyamada <iyamada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 18:26:11 by iyamada           #+#    #+#             */
-/*   Updated: 2022/01/21 18:28:59 by iyamada          ###   ########.fr       */
+/*   Updated: 2022/05/09 17:07:06 by iyamada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static size_t	count_size_except_null(char const *s, char c)
+{
+	size_t	res;
+
+	res = 0;
+	while (*s != '\0')
+	{
+		if (*s == c)
+			res++;
+		s++;
+	}
+	return (res + 1);
+}
+
+static long long	find(const char *s, const char c)
+{
+	long long	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
 char	**ft_split2(char const *s, char c)
 {
-	char	**split_strs;
+	char		**strs;
+	size_t		size;
+	size_t		i;
+	long long	idx;
 
-	if (*s == '\0' && c != '\0')
+	size = count_size_except_null(s, c);
+	i = 0;
+	strs = malloc((size + 1) * sizeof(char *));
+	strs[size] = NULL;
+	while (i < size)
 	{
-		split_strs = (char **)malloc(2 * sizeof(char));
-		split_strs[0] = "\0";
-		split_strs[1] = NULL;
-		return (split_strs);
+		idx = find(s, c);
+		if (idx == -1)
+		{
+			strs[i] = ft_substr(s, 0, ft_strlen(s));
+			break ;
+		}
+		strs[i] = ft_substr(s, 0, (size_t)idx);
+		i++;
+		s = s + (size_t)idx + 1;
 	}
-	return (ft_split(s, c));
+	return (strs);
 }
